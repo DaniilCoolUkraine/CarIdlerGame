6631bb531238d6b3d15d574f6746437ab4840a3d
VIRTUOSGAMESdKrasnikov <dkrasnikov@virtuosgames.com>
2025-05-10 13:18:09 +0300

    added zoom juoce polish

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,6 +1,10 @@
-﻿using SimpleEventBus.SimpleEventBus.Runtime;
+﻿using System.Collections;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using SimpleEventBus.SimpleEventBus.Runtime;
 using SimpleGame.Events;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -9,7 +13,14 @@ namespace SimpleGame
     public class CarController : MonoBehaviour
     {
         [SerializeField, Required] private SplineAnimate _follower;
-
+        
+        [SerializeField, Required] private ParticleSystem[] _fireTrail;
+        [SerializeField] private int _seconds = 1;
+        
+        private float _newSpeed;
+        private float _startSpeed;
+        private Coroutine _speedCoroutine;
+        
         private void OnEnable()
         {
             GlobalEvents.AddListener<UpgradeSpeedEvent>(OnUpgradeSpeed);
@@ -22,7 +33,43 @@ namespace SimpleGame
         
         private void OnUpgradeSpeed(UpgradeSpeedEvent ev)
         {
-            _follower.MaxSpeed += 1;
+            if (_speedCoroutine!= null) 
+                StopCoroutine(_speedCoroutine);
+
+            _newSpeed = _follower.MaxSpeed + 1;
+            _startSpeed = _follower.MaxSpeed;
+
+            _speedCoroutine = StartCoroutine(UpdateSpeedSmoothly());
+            ShowFireTrail().Forget();
+        }
+
+        private IEnumerator UpdateSpeedSmoothly()
+        {
+            try
+            {
+                float timeElapsed = 0f;
+
+                while (timeElapsed < _seconds)
+                {
+                    timeElapsed += Time.deltaTime;
+                    float t = timeElapsed / _seconds;
+                    _follower.MaxSpeed = Mathf.Lerp(_follower.MaxSpeed, _newSpeed, t);
+                    yield return null;
+                }
+            }
+            finally
+            {
+                _follower.MaxSpeed = _newSpeed;
+                _speedCoroutine = null;
+            }
+        }
+
+        private async UniTaskVoid ShowFireTrail()
+        {
+            _fireTrail.ForEach(f => f.gameObject.SetActive(true));
+            _fireTrail.ForEach(f => f.Play());
+            await UniTask.Delay(_seconds * 1000);
+            _fireTrail.ForEach(f => f.Stop());
         }
 
 #if UNITY_EDITOR
